e02c5e39297174e6b11db36e2403fb297e3f8f58
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-09-12 21:41:01 +0800

    fix: Provide solution to the destructingArrays quiz

---
+++ README.md
@@ -0,0 +1,4 @@
+# _Destructuring Arrays Quiz_
+#### _September 12, 2019_
+#### _This is my solution to the destructingArrays quiz from Udacity's free ES6 course._
+#### _By Ryan Ben S. Villanueva_
---
+++ destructuringArrays.js
@@ -0,0 +1,22 @@
+// Directions:
+// Use array destructuring to pull out the three colors from the array of things and store them into the variables one, two, and three.
+
+/*
+ * Programming Quiz: Destructuring Arrays (1-3)
+ *
+ * Use destructuring to initialize the variables `one`, `two`, and `three`
+ * with the colors from the `things` array.
+ */
+
+const things = ['red', 'basketball', 'paperclip', 'green', 'computer', 'earth', 'udacity', 'blue', 'dogs'];
+
+const [one] = things;
+const [,,,two] = things;
+const [,,,,,,,three] = things;
+
+const colors = `List of Colors
+1. ${one}
+2. ${two}
+3. ${three}`;
+
+console.log(colors);
